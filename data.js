410bcd7e16b9be816a30af04f8e3fbd8ebf27b82
AkshayaAcharya <acharya.akshaya04@gmail.com>
2022-07-11 15:09:09 +0530

    updated experiments

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 let data = [
-    {id: "1", name: "Geo Referencing", path: "GEO/simulation/index.html"},
-    {id: "2", name: "Leaf Area Index", path: "LAI/simulation/index.html"},
-    {id: "3", name: "Cartography", path: "CART/simulation/index.html"}
-]
+  { id: "1", name: "Geo Referencing", path: "GEO/simulation/index.html" },
+  { id: "2", name: "Leaf Area Index", path: "LAI/simulation/index.html" },
+  { id: "3", name: "Cartography", path: "CART/simulation/index.html" },
+  { id: "3", name: "DEM", path: "DEM/simulation/index.html" },
+];
