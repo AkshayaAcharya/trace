0b5e7624972473968aabf931c8a67d4e8334c4fc
AkshayaAcharya <acharya.akshaya04@gmail.com>
2022-07-19 16:05:34 +0530

    updated contents

--- data.js
+++ data.js
@@ -3,5 +3,5 @@ let data = [
   { id: "2", name: "Leaf Area Index", path: "LAI/simulation/index.html" },
   { id: "3", name: "Cartography", path: "CART/simulation/index.html" },
   { id: "4", name: "DEM", path: "DEM/simulation/index.html" },
-  { id: "5", name: "DGPS", path: "DGPS/simulation/index.html" },
+  { id: "5", name: "DGPS", path: "DGPS/index.html" },
 ];
